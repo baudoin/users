15c8afaf1a26b0f04a6c4f7ca2af628410d2d470
Andy Baudoin <andy@emergeinteractive.com>
2014-01-10 14:12:04 -0800

    edit users controller

---
+++ app/controllers/users.js
@@ -0,0 +1,9 @@
+var UsersController = Em.ArrayController.extend({
+  sortProperties: ['name'],
+  sortAscending: true,
+  usersCount: function(){
+    return this.get('model.length');
+  }.property('@each')
+});
+
+export default UsersController;
