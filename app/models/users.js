68d13c242d2c0e30aade59eb4fe1f8ccbb53d3f7
Andy Baudoin <andy@emergeinteractive.com>
2014-01-10 12:25:04 -0800

    initial commit of appkit files

---
+++ app/models/users.js
@@ -0,0 +1,26 @@
+var User = DS.Model.extend({
+  'name'         : DS.attr(),
+  'email'        : DS.attr(),
+  'bio'          : DS.attr(),
+  'avatarUrl'    : DS.attr(),
+  'creationDate' : DS.attr()
+});
+
+User.FIXTURES = [{
+  'id' : 1,
+  'name' : 'Sponge Bob',
+  'email' : 'bob@sponge.com',
+  'bio' : 'Lorem ispum dolor sit amet in voluptate fugiat nulla pariatur.',
+  'avatarUrl' : 'http://jkneb.github.io/ember-crud/assets/images/avatars/sb.jpg',
+  'creationDate' : 'Mon, 26 Aug 2013 20:23:43 GMT'
+}, {
+  'id' : 2,
+  'name' : 'John David',
+  'email' : 'john@david.com',
+  'bio' : 'Lorem ispum dolor sit amet in voluptate fugiat nulla pariatur.',
+  'avatarUrl' : 'http://jkneb.github.io/ember-crud/assets/images/avatars/jk.jpg',
+  'creationDate' : 'Fri, 07 Aug 2013 10:10:10 GMT'
+}
+];
+
+export default User;
---
+++ app/router.js
@@ -0,0 +1,15 @@
+var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
+
+Router.map(function() {
+  this.route('component-test');
+  this.route('helper-test');
+  
+  this.resource('users', function(){
+    this.resource('user', { path:'/:user_id' }, function(){
+      this.route('edit');
+    });
+    this.route('create');
+  });
+});
+
+export default Router;
